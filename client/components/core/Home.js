6c310e7bcfd58d8b6dacb238b67caca658ebdf3e
quoctrung163 <miyucoder@gmail.com>
2021-01-22 17:20:19 +0700

    Users component and route link with :heart:

--- client/components/core/Home.js
+++ client/components/core/Home.js
@@ -4,7 +4,7 @@ import { withStyles } from 'material-ui/styles';
 import Card, { CardContent, CardMedia } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import seashellImg from '../../assets/images/seashell.jpg';
-
+import { Link } from 'react-router-dom';
 const styles = theme => ({
   card: {
     maxWidth: 600,
@@ -24,19 +24,17 @@ class Home extends Component {
   render() {
     const { classes } = this.props
     return (
-      <div>
-        <Card className={classes.card}>
-          <Typography type="headline" component="h2" className={classes.title}>
-            Home Page
+      <Card className={classes.card}>
+        <Typography type="headline" component="h2" className={classes.title}>
+          Home Page
           </Typography>
-          <CardMedia className={classes.media} image={seashellImg} title="Unicorn Shells" />
-          <CardContent>
-            <Typography type="body1" component="p">
-              Welcome to the MERN Skeleton home page.
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
+        <CardMedia className={classes.media} image={seashellImg} title="Unicorn Shells" />
+        <CardContent>
+          <Typography type="body1" component="p">
+            Welcome to the MERN Skeleton home page. <Link to="/users">Users</Link>
+          </Typography>
+        </CardContent>
+      </Card>
     )
   }
 }
--- client/components/routing/MainRouter.js
+++ client/components/routing/MainRouter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../core/Home';
+import Users from '../user/Users';
 
 class MainRouter extends Component {
   render() {
@@ -8,10 +9,11 @@ class MainRouter extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/users" component={Users} />
         </Switch>
       </div>
     )
   }
 }
 
-export default MainRouter
+export default MainRouter;
