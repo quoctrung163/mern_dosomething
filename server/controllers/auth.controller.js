801628d5720aedd768fcecc612ce0570a89274a4
quoctrung163 <miyucoder@gmail.com>
2021-01-23 09:33:32 +0700

    fix 403 response status for auth fail in hasAuthorization Controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -29,7 +29,7 @@ const signin = (req, res) => {
 
     return res.json({
       token,
-      user: {_id: user._id, name: user.name, email: user.email}
+      user: { _id: user._id, name: user.name, email: user.email }
     })
 
   })
@@ -50,7 +50,7 @@ const requireSignin = expressJwt({
 const hasAuthorization = (req, res, next) => {
   const authorized = req.profile && req.auth && req.profile._id == req.auth._id
   if (!(authorized)) {
-    return res.status('401').json({
+    return res.status('403').json({
       error: "User is not authorized"
     })
   }
